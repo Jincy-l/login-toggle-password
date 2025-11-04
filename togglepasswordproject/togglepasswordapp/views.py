e444909d2f6876c0150b1c9a3fbfa95aee5dc4f2
jincyl <jincydhiya2022@gmail.com>
2025-11-04 19:52:27 +0530

    register

--- togglepasswordproject/togglepasswordapp/views.py
+++ togglepasswordproject/togglepasswordapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request,'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
--- togglepasswordproject/togglepasswordproject/settings.py
+++ togglepasswordproject/togglepasswordproject/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'togglepasswordproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'togglepasswordproject',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+
+        'PORT':'3306',
     }
 }
 
